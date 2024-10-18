1f8a3f77ad3137a12b654899d4a34619cd1ba553
Computery <77170025+C0mputery@users.noreply.github.com>
2024-10-17 21:50:05 -0400

    a

--- AutoUnityGuidRemapper/Program.cs
+++ AutoUnityGuidRemapper/Program.cs
@@ -4,28 +4,33 @@ namespace AutoUnityGuidRemapper;
 
 static class Program
 {
-    public static void WaitForUserInputAndQuit() { Console.WriteLine("Press any key to continue..."); Console.ReadKey(); Environment.Exit(0); }
-    public static void WaitForUserInput() { Console.WriteLine("Press any key to continue..."); Console.ReadKey();}
+    private static void WaitForUserInputAndQuit() { Console.WriteLine("Press any key to continue..."); Console.ReadKey(); Environment.Exit(0); }
+    private static void WaitForUserInput() { Console.WriteLine("Press any key to continue..."); Console.ReadKey();}
 
-    public static void FindFilesAndFixStucture() {
-        string assetsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Assets");
+    private static void FindFilesAndFixStructure() {
+        Console.WriteLine("STRAFTAT Auto Ripper");
+        Console.WriteLine("Automagically create a working Unity project.");
+        Console.WriteLine("What is the root directory of the Unity project?");
+        string? rootDirectory = Console.ReadLine();
+        if (rootDirectory == null || !Directory.Exists(rootDirectory)) { Console.WriteLine("The directory does not exist!"); WaitForUserInputAndQuit(); return; }
+        
+        string assetsDirectory = Path.Combine(rootDirectory, "Assets");
         if (!Directory.Exists(assetsDirectory)) { Console.WriteLine("Failed to find the Asset folder!?"); WaitForUserInputAndQuit(); return; }
-
+        
         string packageCacheDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Library\\PackageCache");
         if (!Directory.Exists(packageCacheDirectory)) { Console.WriteLine("Failed to find the PackageCache folder, have you opened the project in Unity yet?"); WaitForUserInputAndQuit(); return; }
-
-        string importedFilesDirectory = Path.Combine(assetsDirectory, "!ImportedAssets");
-        if (!Directory.Exists(importedFilesDirectory)) { Console.WriteLine("Failed to find the ImportedAssets folder make sure you've imported the 3rd party assets into the project."); WaitForUserInputAndQuit(); return; }
-
-        string  exportedFilesDirectory = Path.Combine(assetsDirectory, "!ExportedAssets");
-        if (!Directory.Exists(exportedFilesDirectory))
-        {
+        
+        string exportedFilesDirectory = Path.Combine(assetsDirectory, "!ExportedAssets");
+        if (!Directory.Exists(exportedFilesDirectory)) {
             List<string> badFiles = Directory.GetFileSystemEntries(assetsDirectory).ToList();
-            badFiles.Remove(importedFilesDirectory);
-            badFiles.Remove(exportedFilesDirectory + ".meta");
             Directory.CreateDirectory(exportedFilesDirectory);
             foreach (string badFile in badFiles) { Directory.Move(badFile, Path.Combine(exportedFilesDirectory, Path.GetFileName(badFile))); }
-        } 
+        }
+        
+        string importedFilesDirectory = Path.Combine(assetsDirectory, "!ImportedAssets");
+        if (!Directory.Exists(importedFilesDirectory)) {
+            Directory.CreateDirectory(importedFilesDirectory);
+        }
     }
 
     public static void Main(string[] args)
@@ -35,7 +40,9 @@ static class Program
 
         try
         {
-            FindFilesAndFixStucture();
+            FindFilesAndFixStructure();
+            Console.WriteLine("Import the 3rd party assets into the !ImportedAssets folder.");
+            WaitForUserInput();
         }
         catch (Exception e)
         {
